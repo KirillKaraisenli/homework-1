36b6915fac44c3830a2c4c78d66b710d83770ace
KirillKaraisenli <146224890+KirillKaraisenli@users.noreply.github.com>
2025-01-20 16:13:17 +0200

    up

---
+++ script1.js
@@ -0,0 +1,22 @@
+const age = 14;
+console.log(age);
+
+const name = "Kirill";
+console.log(name);
+
+const isStudent = false;
+console.log(isStudent);
+
+const myString =
+  "Учитесь вчера, живите сегодня, надейтесь на завтра. Главное — не прекращать задавать вопросы. Альберт Эйнштейн";
+console.log(myString);
+
+let myNumber = 8;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+let UserName = prompt("Введіть своє ім'я:");
+alert(`Привіт, ${UserName}`);
